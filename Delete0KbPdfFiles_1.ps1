e0f82d7ca29acb4064c8021a3fac2ec46690ff5b
Neilldt <neill.dutoit@sage.com>
2023-09-13 10:43:39 +0200

    Add a new PowerShell script

---
+++ Delete0KbPdfFiles_1.ps1
@@ -0,0 +1,21 @@
+﻿# Define the directory path where you want to search for PDF files
+
+$directoryPath = "C:\Liquid\output\Invoices"
+
+# Get a list of PDF files with a size of 0KB in the specified directory
+
+$zeroSizePDFFiles = Get-ChildItem -Path $directoryPath -Filter *.pdf | Where-Object { $_.Length -eq 0 } 
+
+# Check if there are any 0KB PDF files to delete
+
+if ($zeroSizePDFFiles.Count -gt 0) {
+
+
+        # Delete each 0KB PDF file
+
+        foreach ($file in $zeroSizePDFFiles) {
+
+            Remove-Item -Path $file.FullName -Force
+
+           }
+            }
