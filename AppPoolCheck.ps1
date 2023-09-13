e8111a352cd6956b6eaa103a39b850f913d9ba1f
Neilldt <neill.dutoit@sage.com>
2023-09-13 10:44:28 +0200

    Add a new PowerShell script

---
+++ AppPoolCheck.ps1
@@ -0,0 +1,19 @@
+﻿$AppPoolNames = @("SageOne.Login", "SageOne.Support", "SageOne.Web")
+
+ 
+
+foreach ($AppPoolName in $AppPoolNames) {
+    # Check the status of the application pool
+    $AppPool = Get-WebAppPoolState -Name $AppPoolName
+
+ 
+
+    if ($AppPool.Value -eq 'Stopped') {
+        Write-Output "The application pool '$AppPoolName' is stopped. Starting it..."
+        Start-WebAppPool -Name $AppPoolName
+        Write-Output "Application pool '$AppPoolName' has been started."
+    }
+    else {
+        Write-Output "The application pool '$AppPoolName' is already running."
+    }
+}
